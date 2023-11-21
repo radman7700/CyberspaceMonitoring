ce7d0f28db54941e044823ce6b0a403595d7e94e
radman7700 <150505085+radman7700@users.noreply.github.com>
2023-11-21 15:39:27 +0330

    Update StatisticsCalculator.php

--- src/Services/StatisticsCalculator.php
+++ src/Services/StatisticsCalculator.php
@@ -4,16 +4,19 @@ namespace Pishgaman\CyberspaceMonitoring\Services;
 
 use Pishgaman\CyberspaceMonitoring\Repositories\TelegramGroupRepository;
 use Pishgaman\CyberspaceMonitoring\Repositories\TelegramMessageRepository;
+use Pishgaman\CyberspaceMonitoring\Repositories\TelegramUsersRepository;
 
 class StatisticsCalculator
 {
     protected $groupRepository;
     protected $messageRepository;
+    protected $UsersRepository;
 
-    public function __construct(TelegramGroupRepository $groupRepository, TelegramMessageRepository $messageRepository)
+    public function __construct(TelegramGroupRepository $groupRepository, TelegramMessageRepository $messageRepository,TelegramUsersRepository $UsersRepository)
     {
         $this->groupRepository = $groupRepository;
         $this->messageRepository = $messageRepository;
+        $this->UsersRepository = $UsersRepository;
     }
 
     public function getTelegramGroupCount()
@@ -30,6 +33,11 @@ class StatisticsCalculator
     {
         return $this->groupRepository->sessionCount();
     }
+
+    public function getTelegramUsersCount()
+    {
+        return $this->UsersRepository->getIdCount();
+    } 
 
     public function ReleaseProcess(array $options)
     {
