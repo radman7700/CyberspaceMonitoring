5532bbfacf60d204698512915fc01bb40a33026a
radman7700 <150505085+radman7700@users.noreply.github.com>
2023-12-04 14:47:02 +0330

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Pishgaman\CyberspaceMonitoring\Controllers\web\HomeController;
-use Pishgaman\CyberspaceMonitoring\Controllers\web\TelegramMessages;
+use Pishgaman\CyberspaceMonitoring\Controllers\web\PayeshTelegramController;
 use Pishgaman\CyberspaceMonitoring\Controllers\web\TelegramController;
 
 Route::middleware(['web','auth'])->group(function () {
@@ -11,8 +11,9 @@ Route::middleware(['web','auth'])->group(function () {
 
 Route::get('/payesh/telegram/group/dashboard', [HomeController::class, 'home'])->name('payesh_dashboard')->middleware(['auth','CheckMenuAccess','web']);
 Route::get('/payesh/telegram/channel/dashboard', [HomeController::class, 'channelHome'])->name('payesh_telegram_channel_dashboard')->middleware(['auth','CheckMenuAccess','web']);
-Route::get('/payesh/telegram//group/list', [TelegramMessages::class, 'messagesList'])->name('payesh_telegram_messages_list')->middleware(['auth','CheckMenuAccess','web']);
-Route::get('/payesh/telegram/channel/list', [TelegramMessages::class, 'telegramChannelMessagesList'])->name('payesh_telegram_channel_messages_list')->middleware(['auth','CheckMenuAccess','web']);
+Route::get('/payesh/telegram//group/list', [PayeshTelegramController::class, 'messagesList'])->name('payesh_telegram_messages_list')->middleware(['auth','CheckMenuAccess','web']);
+Route::get('/payesh/telegram/channel/list', [PayeshTelegramController::class, 'telegramChannelMessagesList'])->name('payesh_telegram_channel_messages_list')->middleware(['auth','CheckMenuAccess','web']);
+Route::get('/payesh/telegram/users/list', [PayeshTelegramController::class, 'userList'])->name('payesh_telegram_user_list')->middleware(['auth','CheckMenuAccess','web']);
 Route::get('/payesh/WordCount', [HomeController::class, 'WordCount'])->name('payesh_word_count')->middleware(['auth','web']);
 
 Route::get('/webservice/CyberspaceMonitoring/telegram/saveNewMsg', [TelegramController::class, 'saveNewMsg'])->middleware(['webservice']);
